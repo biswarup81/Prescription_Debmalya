747565e080606bfb73e7d4943cd6aa3c9c9b3a53
Biswarup <Biswarup@DESKTOP-06JDOHS>
2018-09-18 21:16:26 +0200

    Change

--- add_clinical_impression.php
+++ add_clinical_impression.php
@@ -2,7 +2,7 @@
 require_once "inc/config.php";
 $PRESCRIPTION_ID = $_GET['prescription_id'];
 $TYPE = $_GET['TYPE'];
-
+ 
 //GET ID of the TYPE
 
 $query = "select ID from clinical_impression where TYPE = '$TYPE'";
